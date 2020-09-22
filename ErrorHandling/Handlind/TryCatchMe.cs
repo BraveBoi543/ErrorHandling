360aa991e36ec506dc014184cef11cf273fe979d
Joseph Joestar <skylanders024@gmail.com>
2020-09-22 11:10:42 -0400

    Add project files.

---
+++ ErrorHandling/Handlind/TryCatchMe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ErrorHandling.Handlind
+{
+    class TryCatchMe
+    {
+
+        public static void MyErrors()
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText("bob.txt"))
+                {
+                    Console.WriteLine($"The first line of the text is: {sr.ReadLine()}");
+                    Console.WriteLine($"The second line of the text is: {sr.ReadLine()}");
+
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"The file was not found: {e}");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"The Directory Could not be found: {e}");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"File couldn't be opened: {e}");
+            }
+        }
+
+    }
+}
---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunErrorHandling();
+            Console.ReadKey();
+        }
+
+        static void RunErrorHandling()
+        {
+            Handlind.TryCatchMe.MyErrors();
+        }
+    }
+}
